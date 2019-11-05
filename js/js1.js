c57dca4ebffed47b4e94297b4d6277d151ef8ca8
Agnes Dormal <adormallafuente@udesa.edu.ar>
2019-11-05 19:46:52 -0300

    sdhfew
    ksdfn

--- js/js1.js
+++ js/js1.js
@@ -2,6 +2,7 @@
 window.addEventListener("load",function(){
 
   var API_KEY = "6155fe2039bc62a9217a46c95e05b980";
+  var URL_IMG = "https://image.tmdb.org/t/p/original"
 
   var URL_POPULARES = "popular"
   var URL = "https://api.themoviedb.org/3/tv/"+URL_POPULARES+"?api_key="+API_KEY+"&language=en-US&page=1"
@@ -11,10 +12,19 @@ window.addEventListener("load",function(){
     })
     .then(function(objetoLiteralRespuesta) {
       console.log(objetoLiteralRespuesta);
+      var arrayPopulares = objetoLiteralRespuesta.results
+      var populares = document.querySelector ('.populares')
+      var div = ''
+      console.log(arrayPopulares);
+      for (var i = 0; i < arrayPopulares.length; i++) {
 
-        // <div class="serie">
-        //     <img src="" alt="">
-        // </div>
+
+        div = '<div class="serie">'
+        div +=     '<img src="'+URL_IMG+arrayPopulares[i].poster_path+'" alt="" style="heigth:100px;width:150px;">'
+        div += '</div>'
+
+        populares.innerHTML += div
+      }
     })
 
   var URL_MEJOR_PUNTAJE = "top_rated"
