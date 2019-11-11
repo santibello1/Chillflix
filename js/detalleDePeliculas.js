872090c5affd7db847b2b05771eeae05d767b93e
Santi <sbello@udesa.edu.ar>
2019-11-11 20:41:47 -0300

    casi termino detalles. falta solo el trailer

--- js/detalleDePeliculas.js
+++ js/detalleDePeliculas.js
@@ -8,7 +8,7 @@ var URL_IMG = "https://image.tmdb.org/t/p/original"
 
 console.log(id);
 
-var URL_DETALLE = "https://api.themoviedb.org/3/tv/"+id+"?api_key=6155fe2039bc62a9217a46c95e05b980&language=en-US";
+var URL_DETALLE = "https://api.themoviedb.org/3/tv/"+id+"?api_key=6155fe2039bc62a9217a46c95e05b980&language=es-AR";
 
 var serie = document.querySelector('main')
 
@@ -26,10 +26,15 @@ fetch(URL_DETALLE)
      serie.innerHTML += '<div class="imgSerieContainer"><img class="imgSerie" src="'+URL_IMG+unaSerie.poster_path+'" alt="">'
      serie.innerHTML += '</div>'
      serie.innerHTML += '<p class="parrafo">'+unaSerie.overview+'</p>'
-     serie.innerHTML += '<p class="parrafo">'+"Puntaje:"+''+unaSerie.vote_average+'</p>'
-     serie.innerHTML += '<p class="parrafo">'+"Generos:"+''+unaSerie.genres+'</p>'
-
-
+     serie.innerHTML += '<p class="parrafo">'+"Puntaje:"+' '+unaSerie.vote_average+'</p>'
+     serie.innerHTML += '<p class="parrafo">'+"Generos:"+' '+unaSerie.genres+'</p>'
+     serie.innerHTML += '<p class="parrafo">'+"Fecha de estreno:"+' '+unaSerie.first_air_date+'</p>'
+     serie.innerHTML += '<p class="parrafo">'+"Lenguaje original:"+' '+unaSerie.original_language+'</p>'
+     serie.innerHTML += '<br>'
+     serie.innerHTML += '<br>'
+     serie.innerHTML += '<br>'
+     serie.innerHTML += '<br>'
+console.log(unaSerie);
 
   })
   .catch(function (errores) {
